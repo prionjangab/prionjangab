0721ec4cd137da8586217ecb59e8d9a50f985d5f
prionjangab <98085450+prionjangab@users.noreply.github.com>
2022-03-08 21:36:03 +0800

    Add files via upload

---
+++ flowgorithm.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title>Hello!</title>
+</head>
+
+<body>
+
+<?php
+$name = "PrionJanGab";
+$school = "Asia Pacific College";
+$section = "MI202";
+$age = 20 ;
+
+
+
+
+echo "Hi! my name is " . $name  ;
+echo "<br>";
+echo "I study at " . $school  ;
+echo "<br>";
+echo "Section ". $section  ;
+echo "<br>";
+echo "Age ". $age  ;
+echo "<br>";  
+echo "I Love you"
+
+?>
+
+</body>
+</html>
